6f0b5f46360c160b3d09e92016c0c97dde2a45de
Arshveer Singh <arsh.veer0801@gmail.com>
2025-04-04 22:36:39 -0500

    test

--- src/AnimeList.jsx
+++ src/AnimeList.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 function AnimeList({ animeList }) {
-  if (!animeList || animeList.length === 0) {
-    return null; // Do not render anything if there is no anime list
-  }
+  if (!animeList || animeList.length === 0) return null;
 
   return (
     <div className="anime-list">
       <h2>Your Anime List:</h2>
       <ul>
         {animeList.map((anime, index) => (
-          <li key={index} className="anime-item">
+          <li key={index}>
             <strong>{anime.node.title}</strong> - {anime.list_status.status}
           </li>
         ))}
